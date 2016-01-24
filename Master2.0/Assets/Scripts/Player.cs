c744a31a74121366718910b6a9ebecda824ca05b
danicapok <dpok@ucsc.edu>
2016-01-23 16:50:36 -0800

    added when players step on object

--- Master2.0/Assets/Scripts/Player.cs
+++ Master2.0/Assets/Scripts/Player.cs
@@ -36,12 +36,8 @@ public class Player : MonoBehaviour
     {
         HandleMovments();
         HandleInputs();
-		if (Input.GetKeyDown("f")) {
-
-			StartCoroutine("Fade");
-			///ld return new WaitForSeconds(1.0f);
-			Application.LoadLevel ("ninja");
-		}
+		nearObject ();
+		onTopOfPressurePlates ();
     }
 
     private void HandleMovments()
@@ -70,13 +66,14 @@ public class Player : MonoBehaviour
 
     }
 	IEnumerator Fade() {
-		for (float f = 1f; f >= 0; f -= 0.1f) {
+		for (float f = 1f; f >= 0; f -= 0.01f) {
 			Color c = renderer.material.color;
 			c.a = f;
 			renderer.material.color = c;
 			yield return null;
 		}
-		yield return new WaitForSeconds (3.0f);
+		yield return new WaitForSeconds (1.0f);
+		Application.LoadLevel ("ninja");
 
 
 	}
@@ -86,12 +83,20 @@ public class Player : MonoBehaviour
 
 		for (int i = 0; i < obstacles.Length; ++i) {
 			
-			if (Vector3.Distance (transform.position, obstacles [i].transform.position) <= 3.5f) {
+			if (Vector3.Distance (transform.position, obstacles [i].transform.position) <= 1.5f) {
 				Debug.Log ("testing in here");
 				StartCoroutine("Fade");
+				moveSpeed = 0;
 
 			}
 		}
 	}
 
+	void onTopOfPressurePlates(){
+		
+
+
+	}
+	
 }
+
---
+++ Master2.0/Assets/Scripts/pressureplates.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pressureplates : MonoBehaviour {
+
+	public bool isdown;
+	public float distance;
+	public Collider2D col;
+	// Use this for initialization
+	void Start () {
+		isdown = false;
+		col = GetComponent<Collider2D> ();
+		distance = col.bounds.extents.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (check ()) {
+			Debug.Log ("pressure plates");
+		}
+		checking ();
+	}
+	bool check(){
+		return Physics.Raycast(transform.position, -Vector3.up, distance + 0.1f, 0);
+	}
+
+	void checking(){
+		// Find all colliders that overlap
+		BoxCollider2D myCollider = GetComponent<BoxCollider2D>();
+		Collider2D[] otherColliders = Physics2D.OverlapAreaAll(myCollider.bounds.min, myCollider.bounds.max);
+		
+		// Check for any colliders that are on top
+		bool isUnderneath = false;
+		foreach (var otherCollider in otherColliders)
+		{
+			if (otherCollider.transform.position.y > this.transform.position.y)
+			{
+				isUnderneath = true;
+				break;
+			}
+		}
+		
+		// Take the appropriate action
+		if (!isUnderneath)
+		{
+			Debug.Log("HOORAY!");
+			//Destroy(this.gameObject);
+		}
+		else
+		{
+			Debug.Log("On top of the box");
+		}
+	
+	}
+	/*void onTriggerEnter2D(Collider2D col){
+		if (col.gameObject.name == "Player") {
+			isdown = true;
+			Debug.Log ("Pressure Plates pressed");
+		}
+	}*/
+}
